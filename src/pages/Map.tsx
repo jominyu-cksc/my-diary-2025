5a3ea8c4a6cc53b377afd4b1e4b5f11c473d6bf9
Jomin N. Yu <jomin.yu@cksc.edu.ph>
2025-02-05 12:33:45 +0800

    Made the leaflet map to work.
    Installed also missing npm libraries, notably leaflet and -D @types/leaflet (https://react-leaflet.js.org/docs/start-installation/). The center is still a problem though but the browser error is gone.
    
    Then added
    
      import 'leaflet/dist/leaflet.css';
    
    and also this to MapContainer
    
      style={{ width: '100wh', height: 'calc(100vh - 80px)'}}
    
    See: https://stackoverflow.com/questions/40365440/react-leaflet-map-not-correctly-displayed. Now the map is no longer patchy.
    
    Somebody complained that the documentation is really poorly designed.

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,9 +1,12 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
+import 'leaflet/dist/leaflet.css';
+
 function Map() {
-    const position = [51.505, -0.09]
+    const position = { lng: 30, lat: 50 }
     return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}
+            style={{ width: '100wh', height: 'calc(100vh - 80px)'}}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
