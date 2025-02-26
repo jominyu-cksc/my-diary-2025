129cb56a730caaef9f9028c39b6a31a1188a1640
Jomin N. Yu <jomin.yu@cksc.edu.ph>
2025-02-26 11:11:47 +0800

    Made the diary entry data save to memory but there's still minor truncation bug.
    Added validation to Register.tsx

--- src/pages/DiaryEntry.tsx
+++ src/pages/DiaryEntry.tsx
@@ -10,7 +10,13 @@ import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 
+import { format } from 'date-fns/format';
+
 import { DiaryEntryType, moodList, sampleDiary } from '../data/Diary';
+
+export const toDateTimeLocalString = (date: Date) => {
+    return format(date, 'yyyy-MM-dd\'T\'HH:mm:ss')
+}
 
 const regStyle = {
     m: 1,
@@ -20,7 +26,13 @@ const regStyle = {
 function DiaryEntry() {
     const items = sampleDiary
 
-    const [entry, setEntry] = useState<DiaryEntryType | null>(null);
+    const [entry, setEntry] = useState<DiaryEntryType>({
+        id: '',
+        mood: 1,
+        date: new Date(),
+        title: '',
+        content: '',
+    });
 
     const navigate = useNavigate()
     const location = useLocation()
@@ -37,6 +49,16 @@ function DiaryEntry() {
         }
     }, [location])
 
+    const save = () => {
+        if (!entry) {
+            return
+        }
+        if (entry.id === '') {
+            entry.id = (items.length + 1) + ''
+            items.push(entry)
+        }
+    }
+
     return (
         <Box sx={{ width: '95%' }}>
             <h1>Diary Entry</h1>
@@ -46,12 +68,15 @@ function DiaryEntry() {
                     <Select
                         labelId="mood-label"
                         id="mood-select"
-                        value={entry?.id}
+                        value={entry.mood ?? 1}
                         label="Mood"
-                        onChange={() => { }}
+                        onChange={(event) => {
+                            entry.mood = event.target.value as number
+                            setEntry({ ...entry })
+                        }}
                     >
-                        {moodList.map(item => (
-                            <MenuItem value={item.mood}>
+                        {moodList.map((item, index) => (
+                            <MenuItem value={item.mood} key={index}>
                                 <Box component='span' sx={{ fontSize: '1.6em' }}>
                                     {moodList[item.mood - 1].icon}
                                 </Box>
@@ -60,12 +85,44 @@ function DiaryEntry() {
                         ))}
                     </Select>
                 </FormControl>
-                <TextField id="date" label="Date/Time" type='datetime-local' value={entry?.date} variant="outlined" required sx={{ ...regStyle }} />
-                <TextField id="title" label="Title" value={entry?.title} variant="outlined" fullWidth required sx={{ ...regStyle }} />
-                <TextField id="content" label="Content" variant="outlined" value={entry?.content} fullWidth required sx={{ ...regStyle }} />
+                <TextField
+                    id="date"
+                    label="Date/Time"
+                    type='datetime-local' variant="outlined"
+                    value={toDateTimeLocalString(entry?.date ?? new Date())}
+                    onChange={(event) => {
+                        entry.date = new Date(event.target.value)
+                        setEntry({ ...entry })
+                    }}
+                    required
+                    sx={{ ...regStyle }} />
+                <TextField
+                    id="title"
+                    label="Title"
+                    variant="outlined"
+                    value={entry?.title}
+                    onChange={(event) => {
+                        entry.title = event.target.value
+                        setEntry({ ...entry })
+                    }}
+                    fullWidth
+                    required
+                    sx={{ ...regStyle }} />
+                <TextField
+                    id="content"
+                    label="Content"
+                    variant="outlined"
+                    value={entry?.content}
+                    onChange={(event) => {
+                        entry.content = event.target.value
+                        setEntry({ ...entry })
+                    }}
+                    fullWidth
+                    required
+                    sx={{ ...regStyle }} />
                 <Stack spacing={2} direction="row" sx={{ ...regStyle }}>
                     <Button variant="contained" onClick={() => navigate('/')}>Cancel</Button>
-                    <Button variant="contained">Save</Button>
+                    <Button variant="contained" onClick={() => save()}>Save</Button>
                 </Stack>
             </form>
         </Box>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { toDateTimeLocalString } from './DiaryEntry';
+import { Typography } from '@mui/material';
 
 const regStyle = {
     m: 1,
@@ -12,7 +15,27 @@ const regStyle = {
 
 function Register() {
 
+    const [entry, setEntry] = useState({
+        name: '',
+        email: '',
+        birthday: new Date(),
+        password: '',
+        retypePassword: '',
+    });
+    const [error, setError] = useState('')
+
     const navigate = useNavigate();
+
+    const validate = () => {
+        if (entry.email === '') {
+            setError('Email is required')
+            return
+        }
+        if (entry.password !== entry.retypePassword) {
+            setError("Passwords didn't match")
+            return
+        }
+    }
 
     return <Box
         component="form"
@@ -22,15 +45,64 @@ function Register() {
     >
         <h1>Register</h1>
         <form>
-            <TextField id="name" label="Name" variant="outlined" required sx={{ ...regStyle }} />
-            <TextField id="email" label="Email" type='email' variant="outlined" required sx={{ ...regStyle }} />
-            <TextField id="password" label="Password" type='password' variant="outlined" required sx={{ ...regStyle }} />
-            <TextField id="password2" label="Retype password" type='password' variant="outlined" required sx={{ ...regStyle }} />
-            <TextField id="birthday" label="Birthday" type='date' variant="outlined" required sx={{ ...regStyle }} />
+            <TextField
+                id="name"
+                label="Name"
+                variant="outlined"
+                onChange={(event) => {
+                    entry.name = event.target.value
+                    setEntry({ ...entry })
+                }}
+                required
+                sx={{ ...regStyle }} />
+            <TextField
+                id="email"
+                label="Email"
+                type='email'
+                variant="outlined"
+                onChange={(event) => {
+                    entry.email = event.target.value
+                    setEntry({ ...entry })
+                }}
+                required
+                sx={{ ...regStyle }} />
+            <TextField
+                id="password"
+                label="Password"
+                type='password'
+                variant="outlined"
+                onChange={(event) => {
+                    entry.password = event.target.value
+                    setEntry({ ...entry })
+                }}
+                required
+                sx={{ ...regStyle }} />
+            <TextField
+                id="password2"
+                label="Retype password"
+                type='password'
+                variant="outlined"
+                onChange={(event) => {
+                    entry.retypePassword = event.target.value
+                    setEntry({ ...entry })
+                }}
+                required sx={{ ...regStyle }} />
+            <TextField
+                id="birthday"
+                label="Birthday"
+                type='datetime-local' variant="outlined"
+                value={toDateTimeLocalString(entry.birthday ?? new Date())}
+                onChange={(event) => {
+                    entry.birthday = new Date(event.target.value)
+                    setEntry({ ...entry })
+                }}
+                required
+                sx={{ ...regStyle }} />
             <Stack spacing={2} direction="row" sx={{ ...regStyle }}>
                 <Button variant="contained" onClick={() => navigate('/')}>Cancel</Button>
-                <Button variant="contained">Register</Button>
+                <Button variant="contained" onClick={() => validate()}>Register</Button>
             </Stack>
+            <Typography color='error'>{error}</Typography>
         </form>
     </Box>
 }
